bc490f31cc7efea9e3b4489a9dcb21daa8e8e7a9
Zeyu Yuan <798745160@qq.com>
2019-05-16 21:26:07 +0800

    Little again

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ class Assignment(db.Model):
 	target = db.Column(db.String(60))
 	description = db.Column(db.String(200))
 	ddl = db.Column(db.String(10))
+	state = db.Column(db.Boolean, default=False)
 
 @app.cli.command()
 @click.option('--drop', is_flag=True, help='Create after drop.')
@@ -51,6 +52,14 @@ def forge():
 	db.session.commit()
 	click.echo('Done.')
 
+def if_ddl(ddl):
+	tmp = ddl.split('-')
+	if int(tmp[-2]) < 1 or int(tmp[-2]) > 12:
+		return False
+	if int(tmp[-1]) < 1 or int(tmp[-1]) > 31:
+		return False
+	return True
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
@@ -59,7 +68,7 @@ def index():
 		if not description:
 			description = 'None'
 		ddl = request.form.get('ddl')
-		if not target or not ddl or len(ddl) > 10 or len(target) > 60:
+		if not target or not ddl or len(ddl) > 10 or len(target) > 60 or not if_ddl(ddl):
 			flash('Invalid input.')
 			return redirect(url_for('index'))
 		assignment = Assignment(target=target, description=description, ddl=ddl)
@@ -84,3 +93,39 @@ def inject_user():
 	user = User.query.first()
 	return dict(user=user)
 
+@app.route('/change/<int:hw_id>', methods=['POST'])
+def change(hw_id):
+	hw = Assignment.query.get_or_404(hw_id)
+	hw.state = not hw.state
+	db.session.commit()
+	flash("Hw's state has changed.")
+	return redirect(url_for('index'))
+
+@app.route('/edit/<int:hw_id>', methods=['GET', 'POST'])
+def edit(hw_id):
+	hw = Assignment.query.get_or_404(hw_id)
+	if request.method == 'POST':
+		target = request.form.get('target')
+		description = request.form.get('description')
+		ddl = request.form.get('ddl')
+		
+		if not target or not ddl or len(ddl) > 10 or len(target) > 60 or not if_ddl(ddl):
+			flash('Invalid input.')
+			return redirect(url_for('edit', hw_id=hw_id))
+
+		hw.target = target
+		hw.description = description
+		hw.ddl = ddl
+		db.session.commit()
+		flash('Item updated.')
+		return redirect(url_for('index'))
+
+	return render_template('edit.html', hw=hw)
+
+@app.route('/delete/<int:hw_id>', methods=['POST'])
+def delete(hw_id):
+	hw = Assignment.query.get_or_404(hw_id)
+	db.session.delete(hw)
+	db.session.commit()
+	flash('Item deleted.')
+	return redirect(url_for('index'))
